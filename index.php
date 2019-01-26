b64a4286ba9c5b59670a39e8878bcba748141065
zcoder <dusrksl@gmail.com>
2019-01-26 11:29:31 +0900

    2019.01.26 version 1.0

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <?php echo get_template_directory_uri();?>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,3 @@
+<?php
+ /* Template Name: Company */ 
+ get_header(); ?>
