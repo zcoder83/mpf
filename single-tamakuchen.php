25cc458cde8bc9f02b4ecb22dd3d0130ecbe495c
zcoder <dusrksl@gmail.com>
2019-02-02 21:11:51 +0900

    2019.02.02 version 1.0

---
+++ single-tamakuchen.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Template Name: Work Tamakuchen
+ * Template Post Type: post, page, product
+ */
+get_header();
+?>
+<div class="tamakuchen-content">
+    <div class="back-home clearfix"><a class="logo" href="<?php echo get_home_url(); ?>"><img src="<?php bloginfo('template_url'); ?>/img/logo-b.png" alt="logo"></a><a class="go-front" href="<?php echo home_url(); ?>">BACK HOME<span></span></a></div>
+    <table class="pf-work">
+        <tr>
+            <td class="title right-100">YEAR</td>
+            <td class="title right-150">DELIVERABLES</td>
+            <td class="pf-th">TakaKuchen</td>
+            
+        </tr>
+        <tr>
+            <td class="desc right-100">2018</td>
+            <td class="desc right-150">UX<br><br>Visual Design</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td class="pf-desc">We were asked by BASIC to help them develop a new look and feel for NERD Skincare. Working closely with the SD team we developed several concepts that covered the e-commerce platform, branded content, and tone of voice. We then created a suite of brand assets as a toolkit that could be used by the NERD marketing team in their communications.</td>
+        </tr>
+    </table>
+    <div class="pf-img"><img src="<?php bloginfo('template_url'); ?>/img/tamakuchen.jpg" alt=""></div>
+    <div class="stand-out clearfix">
+        <div class="title">Stand out from<br>the crowd</div>
+        <div class="desc">We were asked by BASIC to help them develop a new look and feel for NERD Skincare. Working closely with the SD team we developed several concepts that covered the e-commerce platform, branded content, and tone of voice. We then created a suite of brand assets as a toolkit that could be used by the NERD marketing team in their communications.</div>
+    </div>
+    <div class="work-img">
+        <div class="work-wrapper">
+            <img src="<?php bloginfo('template_url'); ?>/img/tamakuchen-work.png" alt="firstclass-work">
+        </div>
+    </div>
+</div>
+<footer class="ft-tamakuchen">
+    <div class="footer">
+        <div class="prev-project">
+            <a href="<?php echo home_url('/First-class');?>" alt="prev"><span class="prev"></span>PREV PROJECT</a>
+        </div>
+        <div class="footer-work"><span>Work</span></div>
+        <div class="next-project">
+            <a href="<?php echo home_url('/Cebu-IT-Abroad');?>" alt="next">NEXT PROJECT<span class="next"></span></a>
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- work.php
+++ work.php
@@ -11,25 +11,20 @@ as a web designer, my goal is to create well thought & beautiful interfaces to o
     <div class="projects">
         <p class="select-p">SELECT PROJECTS</p>
         <span>/</span>
-        <div class="project-item">
-            <a class="project-img" href="<?php permalink_link(); ?>"><img src="<?php bloginfo('template_url'); ?>/img/project-img.png" alt=""></a>
-            <h2><a href="<?php permalink_link(); ?>">Cebu IT Abroad</a></h2>
-        </div>
         <?php 
         // the query
         $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
         
         <?php if ( $wpb_all_query->have_posts() ) : ?>
-        
-        <ul>
-        
             <!-- the loop -->
             <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <div class="project-item">
+            <a class="project-img" href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'medium' ); ?></a>
+            <h2><a href="<?php permalink_link(); ?>"><?php the_title(); ?></a></a></h2>
+            
+            </div>
             <?php endwhile; ?>
             <!-- end of the loop -->
-        
-        </ul>
         
             <?php wp_reset_postdata(); ?>
         
